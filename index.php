e3a7fd7f8f189604afd932794288056ea2e4b206
Gerrit Van Dyke <gvandyke1@cnm.edu>
2017-04-10 11:38:01 -0600

    about me website with image

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title>About Gerrit</title>
+	</head>
+	<body>
+		<img src="images/profilepic.jpg" alt="my pic" />
+		<p> I'm out here in Albuquerque trying to change the trajectory of my future. For the next 10 weeks, I'll be a bit occupied so you might have a hard time reaching me.
+		</p>
+		<p> The ability to travel and experience new things interests me more than anything else.</p>
+		<p> My favorite team is the Packers so don't get into a discussion with me about football unless you know what you're talking about.</p>
+	</body>
+<</html>
